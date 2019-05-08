6b162f2869704d27f83ac5d29e9b9fa523e6591b
peckhouse <millennionprandi@gmail.com>
2019-05-08 18:21:41 +0900

    :recycle: Add rand number + correction on index.php

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php include('../includes/_head.php') ?>
+    <?php include('config.php') ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+
+    <title>Test website</title>
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <script src="//j.dev-wovn.io:3000/1" data-wovnio="key=<?php echo $wovn_key ?>" async></script>
   </head>
   <body>
     <?php include('includes/_header.php') ?>
