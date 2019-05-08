34cc0c854a78d6fddfd6c58b42aa996c33e510ea
peckhouse <millennionprandi@gmail.com>
2019-05-08 17:49:17 +0900

    :sparkles: Add dynamic contents page.

--- includes/_dynamic-content-1.php
+++ includes/_dynamic-content-1.php
@@ -1,1 +1,49 @@
-<p class="test">CONTENT!!!!!</p>
+<div class="dynamic-import-1">
+  <p class="text">か模舳く離雲保氏夜個差夜差差都ムネウテウむ目都以課個尾夜模すも尾へら離擢留野く譜列そほむ日個え、つね雲阿きおせ保露せそそ御雲根おうくる瀬、絵二等巣絵夜まササトマ課遊等魔コュネ模個留露巣屋ふめ、せひいあれ氏擢課御手屋模知留り根、むやよおねやんょっせや瀬巣二雲舳根ちめょっらょとむ魔二。くヒロらら。</p>
+
+  <div class="text">よょ二個目差魔巣日露列つひやないせ二毛雲けんめつ他擢阿さた「け都樹」よきそ津氏離保日他ちゅはこは以樹れくめろれさレメヤイスイョュマウ。もつぬら模目ょれな派露根きし魔魔鵜く留保譜えゃも素日素んそくうュテカシ野根留都こねけさゃ樹リセホクャニ名列擢瀬モュヘョム、ネャチヘャたむこま以屋ぬみへ離魔に、み名津尾根御目み遊阿絵樹絵うしやきす津そちけ模模、めけな巣絵二目魔絵野もぬの野個根ち。</div>
+
+  <ul class="text">
+    <li>ゆぬあ絵毛鵜区都他絵尾とね、個タホヘ瀬素離尾保譜模んなさ模保ちちえましねふゆち派保差樹屋差すあせこ二ゆ阿個ねはる巣素メンオん等は</li>
+    <li>しノセ都野保日れてもみゅ遊ナョトコラ露個へふらユソオアヤクロおき毛より無列夜っこ派露他お</li>
+    <li>のるみ鵜毛都て譜都尾こせす日雲野ヘルホンフノナょす</li>
+    <li>根阿へもい他<em>「差ゆく」</em>の絵遊等舳おひせ鵜鵜素い。</li>
+  </ul>
+
+  <span class="text">
+    <span>ゆ無巣とか</span>
+    <span>えおつふめ離屋オユムノルヘュ。</span>
+    <span>名ふかき絵留りりね樹保えはっむりゃ</span>
+    <span>も個うくきあっゃす。</span>
+    <span>むにゅり模氏瀬素二よろ</span>
+    <b>にるたそろ絵等模みめたむれん。</b>
+    <span>みセツソ夜ゃ日譜他巣に。</span>
+    <span>手名。め素等ま舳野魔名いうし、</span>
+    <small>魔手知ぬせ個瀬そりゆち毛二。</small><br><br>
+    <span>等無キヨ</span>
+    <i>モ手留魔</i>
+    <span>しあめふ知っっ、</span>
+    <span>ふよなるえ</span>
+    <u>巣列個離保</u>
+    <span>やめふれせろ</span>
+    <span>露ょえッメ</span>
+    <strong>オラフハテラウャ</strong>
+    <span>、以名御氏、</span>
+    <span>セサッ</span>
+    <em>。</em>
+  </span>
+
+  <p class="text">毛瀬アノンリャクョモラュヤせねにて根知ろえ列舳譜都んさうふくにねそとま個保譜ひヨソスハカモユ保譜「保ツノ」ソリテ区個はろこれ、<small>舳他津津れくこらな二擢うみ留他樹て野模よろ、以樹かゆる津よほら素譜保かよちっトク根保素模個御。</small></p>
+  <img class="image image--25" src="https://via.placeholder.com/1280x120?text=Test+image+number+1" alt="">
+
+  <figure>
+    <img class="image" src="https://via.placeholder.com/1280x120?text=Test+image+number+2" alt="">
+    <figcaption class="text">
+      ん樹雲樹等に阿二尾ヌレエヨッ。ほょゃほふ野魔、ねさね遊魔舳すえお個列区ぬ他以津野差舳イセメ目尾列ひ目鵜個舳むっ露テニムおめそまあにぬえなく夜目巣むす。
+    </figcaption>
+  </figure>
+
+
+  <img class="image image--50" src="https://via.placeholder.com/1280x120?text=Test+image+number+3" alt="">
+  <iframe id="sHframe" width="500" height="500" style="overflow: auto;" src="nested-iframe.php"></iframe>
+</div>
--- javascript/dynamic-contents.js
+++ javascript/dynamic-contents.js
@@ -1,19 +1,12 @@
 $(document).ready(function () {
-  console.log('YES');
-
-
   $('#button-1').on('click', function () {
     $('#loader-1').addClass('.loading');
 
-
     $.ajax( "../includes/_dynamic-content-1.php" )
       .done(function(data) {
-        console.log("success", data);
-
-        $('.dynamic-content-1').html(data)
         setTimeout(function() {
-          $('#loader-1').removeClass('.loading');
-        }, 2000)
+          $('.dynamic-content-1').html(data)
+        }, 1000)
       });
   });
 })
